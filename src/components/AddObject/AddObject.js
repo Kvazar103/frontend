39346fa61209753984534f1910e33234eaa528d4
Kvazar103 <nazar.voinarovych250@gmail.com>
2023-03-19 14:11:26 +0200

    up

--- src/components/AddObject/AddObject.js
+++ src/components/AddObject/AddObject.js
@@ -37,37 +37,37 @@ export default function AddObject(){
         setAddress(e.target.value);
     }
     const onDistinctChange = (e) => {
-      setDistinct(e.target.value);
+        setDistinct(e.target.value);
     }
     const onRoomsChange = (e) => {
         setRooms(e.target.value);
     }
     const onSquareChange = (e) => {
-      setSquare(e.target.value);
+        setSquare(e.target.value);
     }
     const onRealEstateChange = (e) => {
-      setReal_Estate(e.target.value);
+        setReal_Estate(e.target.value);
     }
     const onPriceSumChange = (e) => {
-      setPrice_sum(e.target.value);
+        setPrice_sum(e.target.value);
     }
     const onPriceCurrency = (e) => {
-      setPrice_currency(e.target.value);
+        setPrice_currency(e.target.value);
     }
     const onPriceTypeOfOrderOfRealEstate = (e) => {
-      setPrice_type_of_order_of_real_estate(e.target.value);
+        setPrice_type_of_order_of_real_estate(e.target.value);
     }
     const onImagesChange = (e) => {
-      setImages(e.target.files);
+        setImages(e.target.files);
     }
 
     const handleSave =async (e) => {
-      e.preventDefault();
+        e.preventDefault();
         const customer=JSON.parse(localStorage.getItem("customer"));
-      // await axios.post(`http://localhost:8080/${customer.id}/addObject`,object)
+        // await axios.post(`http://localhost:8080/${customer.id}/addObject`,object)
 
-      //   navigate("/object/:id")
-      //   window.location.reload()
+        //   navigate("/object/:id")
+        //   window.location.reload()
         AuthService.saveObject(address,distinct,rooms,square,real_estate,price_sum,price_currency,price_type_of_order_of_real_estate,images)
             .then(()=>{
                 // navigate("/");
@@ -131,119 +131,119 @@ export default function AddObject(){
             <br/><br/><br/><br/>
             <div className="row">
                 <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-        <form className="row g-3" onSubmit={(e)=>handleSave(e)}>
-            <div className="col-12">
-                <label htmlFor="inputAddress" className="form-label">Address</label>
-                <input type="text" className="form-control" name="address" value={address} onChange={(e) => onAddressChange(e)} id="inputAddress" placeholder="1234 Main St"/>
-            </div>
-            <div className="col-12">
-                <label htmlFor="inputAddress2" className="form-label">Distinct</label>
-                <input type="text" className="form-control" name="distinct" value={distinct} onChange={(e) => onDistinctChange(e)} id="inputAddress2"
-                       placeholder="Apartment, studio, or floor"/>
-            </div>
-            <div className="col-md-4">
-                <label htmlFor="inputEmail4" className="form-label">Rooms</label>
-                <input type="number" className="form-control" name="rooms" value={rooms} onChange={(e) => onRoomsChange(e)} id="inputEmail4"/>
-            </div>
-            <div className="col-md-4">
-                <label htmlFor="inputPassword4" className="form-label">Square</label>
-                <input type="number" className="form-control" name="square" value={square} onChange={(e) => onSquareChange(e)} id="inputPassword4"/>
-            </div>
-            <div className="col-md-4">
-                <label htmlFor="inputZip" className="form-label">Real estate</label>
-                <select id="inputState2" name="real_estate" value={real_estate} onChange={(e) => onRealEstateChange(e)} className={css.d} >
-                    <option defaultValue={""}>Choose...</option>
-                    <option>...</option>
-                    <option value="Apartment">Apartment</option>
-                    <option value="House">House</option>
-                    <option value="Garage">Garage</option>
-                    <option value="Land">Land</option>
-                </select>
-            </div>
-            <div className="col-12"><br/>
-                Price
-            </div>
-            <div className="col-md-6">
-                <label htmlFor="inputCity" className="form-label">Sum</label>
-                <input type="number" name="sum" value={price_sum} onChange={(e) => onPriceSumChange(e)}  className="form-control" id="inputCity"/>
-            </div>
-            <div className="col-md-2">
-                <label htmlFor="inputZip" className="form-label">Currency</label>
-                <select id="inputState2" name="currency" value={price_currency} onChange={(e) => onPriceCurrency(e)} className={css.c} >
-                    <option defaultValue={""}>Choose...</option>
-                    <option>...</option>
-                    <option value="EUR">EUR</option>
-                    <option value="USD">USD</option>
-                    <option value="UAH">UAH</option>
-                </select>
-            </div>
-            <div className="col-md-4">
-                <label htmlFor="inputState" className="form-label">Type of real estate</label>
-                <select id="inputState" className={css.b} name="type_of_real_estate" value={price_type_of_order_of_real_estate} onChange={(e) => onPriceTypeOfOrderOfRealEstate(e)}>
-                    <option defaultValue={""}>Choose...</option>
-                    <option>...</option>
-                    <option value="Sell">Sell</option>
-                    <option value="Rent_for_a_month">Rent for a month</option>
-                    <option value="Rent_per_day">Rent per day</option>
-                </select>
-            </div>
-            {/*<div>*/}
-            {/*    <div>*/}
-            {/*        add photos*/}
-            {/*    </div>*/}
-            {/*    <input type="file"/>*/}
-            {/*</div>*/}
-            <section>
-                <label>
-                    + Add Images
-                    <br />
-                    <span>up to 10 images</span>
-                    <input
-                        type="file"
-                        name="images"
-                        onChange={onImagesChange}
-                        value={images}
-                        multiple
-                        accept="image/png , image/jpeg, image/webp"
-                    />
-                </label>
-                <br />
+                    <form className="row g-3" onSubmit={(e)=>handleSave(e)}>
+                        <div className="col-12">
+                            <label htmlFor="inputAddress" className="form-label">Address</label>
+                            <input type="text" className="form-control" name="address" value={address} onChange={(e) => onAddressChange(e)} id="inputAddress" placeholder="1234 Main St"/>
+                        </div>
+                        <div className="col-12">
+                            <label htmlFor="inputAddress2" className="form-label">Distinct</label>
+                            <input type="text" className="form-control" name="distinct" value={distinct} onChange={(e) => onDistinctChange(e)} id="inputAddress2"
+                                   placeholder="Apartment, studio, or floor"/>
+                        </div>
+                        <div className="col-md-4">
+                            <label htmlFor="inputEmail4" className="form-label">Rooms</label>
+                            <input type="number" className="form-control" name="rooms" value={rooms} onChange={(e) => onRoomsChange(e)} id="inputEmail4"/>
+                        </div>
+                        <div className="col-md-4">
+                            <label htmlFor="inputPassword4" className="form-label">Square</label>
+                            <input type="number" className="form-control" name="square" value={square} onChange={(e) => onSquareChange(e)} id="inputPassword4"/>
+                        </div>
+                        <div className="col-md-4">
+                            <label htmlFor="inputZip" className="form-label">Real estate</label>
+                            <select id="inputState2" name="real_estate" value={real_estate} onChange={(e) => onRealEstateChange(e)} className={css.d} >
+                                <option defaultValue={""}>Choose...</option>
+                                <option>...</option>
+                                <option value="Apartment">Apartment</option>
+                                <option value="House">House</option>
+                                <option value="Garage">Garage</option>
+                                <option value="Land">Land</option>
+                            </select>
+                        </div>
+                        <div className="col-12"><br/>
+                            Price
+                        </div>
+                        <div className="col-md-6">
+                            <label htmlFor="inputCity" className="form-label">Sum</label>
+                            <input type="number" name="sum" value={price_sum} onChange={(e) => onPriceSumChange(e)}  className="form-control" id="inputCity"/>
+                        </div>
+                        <div className="col-md-2">
+                            <label htmlFor="inputZip" className="form-label">Currency</label>
+                            <select id="inputState2" name="currency" value={price_currency} onChange={(e) => onPriceCurrency(e)} className={css.c} >
+                                <option defaultValue={""}>Choose...</option>
+                                <option>...</option>
+                                <option value="EUR">EUR</option>
+                                <option value="USD">USD</option>
+                                <option value="UAH">UAH</option>
+                            </select>
+                        </div>
+                        <div className="col-md-4">
+                            <label htmlFor="inputState" className="form-label">Type of real estate</label>
+                            <select id="inputState" className={css.b} name="type_of_real_estate" value={price_type_of_order_of_real_estate} onChange={(e) => onPriceTypeOfOrderOfRealEstate(e)}>
+                                <option defaultValue={""}>Choose...</option>
+                                <option>...</option>
+                                <option value="Sell">Sell</option>
+                                <option value="Rent_for_a_month">Rent for a month</option>
+                                <option value="Rent_per_day">Rent per day</option>
+                            </select>
+                        </div>
+                        {/*<div>*/}
+                        {/*    <div>*/}
+                        {/*        add photos*/}
+                        {/*    </div>*/}
+                        {/*    <input type="file"/>*/}
+                        {/*</div>*/}
+                        <section>
+                            <label>
+                                + Add Images
+                                <br />
+                                <span>up to 10 images</span>
+                                <input
+                                    type="file"
+                                    name="images"
+                                    onChange={onImagesChange}
+                                    value={images}
+                                    multiple
+                                    accept="image/png , image/jpeg, image/webp"
+                                />
+                            </label>
+                            <br />
 
 
 
-            {/*    {selectedImages.length>10?(          <p className="error">*/}
-            {/*        You can't upload more than 10 images! <br />*/}
-            {/*        <span>*/}
-            {/*  please delete <b> {selectedImages.length - 10} </b> of them{" "}*/}
-            {/*</span>*/}
-            {/*    </p>):<div></div>}*/}
+                            {/*    {selectedImages.length>10?(          <p className="error">*/}
+                            {/*        You can't upload more than 10 images! <br />*/}
+                            {/*        <span>*/}
+                            {/*  please delete <b> {selectedImages.length - 10} </b> of them{" "}*/}
+                            {/*</span>*/}
+                            {/*    </p>):<div></div>}*/}
 
-            {/*    <div className="images">*/}
-            {/*        {selectedImages &&*/}
-            {/*            selectedImages.map((image, index) => {*/}
-            {/*                return (*/}
-            {/*                    <div key={image} className="image">*/}
-            {/*                        <img src={image} height="200" alt="upload" />*/}
-            {/*                        <button onClick={() => deleteHandler(image)}>*/}
-            {/*                            delete image*/}
-            {/*                        </button>*/}
-            {/*                        <p>{index + 1}</p>*/}
-            {/*                    </div>*/}
-            {/*                );*/}
-            {/*            })}*/}
-            {/*    </div>*/}
-            </section>
+                            {/*    <div className="images">*/}
+                            {/*        {selectedImages &&*/}
+                            {/*            selectedImages.map((image, index) => {*/}
+                            {/*                return (*/}
+                            {/*                    <div key={image} className="image">*/}
+                            {/*                        <img src={image} height="200" alt="upload" />*/}
+                            {/*                        <button onClick={() => deleteHandler(image)}>*/}
+                            {/*                            delete image*/}
+                            {/*                        </button>*/}
+                            {/*                        <p>{index + 1}</p>*/}
+                            {/*                    </div>*/}
+                            {/*                );*/}
+                            {/*            })}*/}
+                            {/*    </div>*/}
+                        </section>
 
 
 
-            {/*{selectedImages.length>10?(<div className="col-12">   <br/>*/}
-            {/*    <button type="submit" disabled className="btn btn-primary">Add new object</button>*/}
-            {/*</div>):(*/}
-                <div className="col-12">   <br/>
-                <button type="submit" className="btn btn-primary">Add new object</button>
-            </div>
-            {/*)}*/}
-        </form>
+                        {/*{selectedImages.length>10?(<div className="col-12">   <br/>*/}
+                        {/*    <button type="submit" disabled className="btn btn-primary">Add new object</button>*/}
+                        {/*</div>):(*/}
+                        <div className="col-12">   <br/>
+                            <button type="submit" className="btn btn-primary">Add new object</button>
+                        </div>
+                        {/*)}*/}
+                    </form>
                 </div></div></div>
 
     )
